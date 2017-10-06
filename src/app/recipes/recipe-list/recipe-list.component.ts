60c61c0b80e20c20b760122d76f0b28fbb7971a9
Vsevolod Bondar <vsevolod.bondar@sysgears.com>
2017-10-06 15:08:35 +0300

    Created base project structure

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Simple test recipe', `http://www.coca-colacompany.com/content/dam/journey/us/en/global/2012/11/chicken-wings-604mk112612-604-337-3f7d77f6.rendition.598.336.jpg`),
+    new Recipe('Another Recipe', 'Another simple recipe', `https://www.ndtv.com/cooks/images/620_2.jpg`),
+    new Recipe('The Best Recipe', 'Best recipe', `https://static01.nyt.com/images/2014/08/19/dining/chickenskewers/chickenskewers-superJumbo.jpg`)
+  ];
 
   constructor() { }
 
