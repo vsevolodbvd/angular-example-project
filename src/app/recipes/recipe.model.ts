3477d9a8af4bd09797baf3f9a99d03b58283028b
Vsevolod Bondar <vsevolod.bondar@sysgears.com>
2017-10-06 15:40:55 +0300

    Added Ingredient model. Added shopping list edit template

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,7 +1,7 @@
 export class Recipe {
-  name: string;
-  description: string;
-  imagePath: string;
+  public name: string;
+  public description: string;
+  public imagePath: string;
 
   constructor(name: string, desc: string, imagePath: string) {
     this.name = name;
