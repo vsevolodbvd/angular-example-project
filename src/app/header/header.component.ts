040d163677829c4f3533b66f2353b29ca9fe9f2b
Vsevolod Bondar <vsevolod.bondar@sysgears.com>
2017-10-09 22:51:48 +0300

    Added navigation between Recipes and Shopping list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(pageName: string) {
+    this.featureSelected.emit(pageName);
+  }
 
 }
