794257a99f852b5de0b4caa961bcc879bef188ee
Vsevolod Bondar <vsevolod.bondar@sysgears.com>
2017-10-25 17:29:47 +0300

    Created shopping-list  and auth modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from "@angular/core";
-import {DropdownDirective} from "./dropdown.directive";
 import {CommonModule} from "@angular/common";
+
+import {DropdownDirective} from "./dropdown.directive";
 
 @NgModule({
   declarations: [
---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+
+import {ShoppingEditComponent} from "./shopping-edit/shopping-edit.component";
+import {ShoppingListComponent} from "./shopping-list.component";
+
+@NgModule({
+  declarations: [
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
+})
+export class ShoppingListModule {}
