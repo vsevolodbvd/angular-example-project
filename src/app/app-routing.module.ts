034fb38b5ec50bfb23e44d408e61234c8835a849
Vsevolod Bondar <vsevolod.bondar@sysgears.com>
2017-10-25 17:54:09 +0300

    Implemented lazy loading for recipes module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+import {HomeComponent} from "./home/home.component";
 
 const appRoutes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
