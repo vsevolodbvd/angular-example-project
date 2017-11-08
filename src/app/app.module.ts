c30ef8adc2060c234472750b83244b79adcb4145
Vsevolod Bondar <vsevolod.bondar@sysgears.com>
2017-11-08 21:48:55 +0200

    Configured express server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { environment} from '../environments/environment';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({appId: 'my-universal-app'}),
     HttpClientModule,
     AppRoutingModule,
     SharedModule,
